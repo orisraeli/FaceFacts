7b8eb7843ffb5bcbed66aadaa2a25413a784a916
Or Israeli <isra.or@gmail.com>
2024-01-16 18:22:01 +0200

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Or Israeli on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
